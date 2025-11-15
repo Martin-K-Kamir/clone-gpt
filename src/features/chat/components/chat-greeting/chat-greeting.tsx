0f01410fad733d7589bda458e61186e30d15cc8b
Martin Kamir <martin.k.kamir@gmail.com>
2025-11-16 00:01:43 +0100

    Add framer-motion for animation in ChatGreeting component

--- src/features/chat/components/chat-greeting/chat-greeting.tsx
+++ src/features/chat/components/chat-greeting/chat-greeting.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { motion } from "framer-motion";
 import { memo, useEffect, useState } from "react";
 
 import { AnyComponent } from "@/components/ui/any-component";
@@ -42,15 +43,22 @@ export const ChatGreeting = ({
     }
 
     return (
-        <Comp
-            className={cn(
-                "whitespace-pre-line bg-gradient-to-r from-indigo-500 to-rose-500 bg-clip-text text-3xl font-bold text-transparent",
-                className,
-            )}
-            {...props}
+        <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.1 }}
         >
-            {greeting}
-        </Comp>
+            <Comp
+                className={cn(
+                    "whitespace-pre-line bg-gradient-to-r from-indigo-500 to-rose-500 bg-clip-text text-3xl font-bold text-transparent",
+                    className,
+                )}
+                {...props}
+            >
+                {greeting}
+            </Comp>
+        </motion.div>
     );
 };
 
